918d6618934ea16206956b3cb8a3abf3a3dfab90
ImtehQ <its_q@hotmail.com>
2021-02-09 11:35:02 +0100

    grgd

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,27 +1,18 @@
 ﻿using ItVitae_RepairShop_project8.Models;
 using System.Collections.Generic;
 using System.Web.Mvc;
-using static RepairShopData.Scripts.PartProcessor; 
+using System.Linq;
+using RepairShopData.Scripts;
+using System.Net;
 
 namespace ItVitae_RepairShop_project8.Controllers
 {
-
-    /// Hele project is er wel maar je ziet het niet.
-    /// Aan gezien het hele project een directe pcopy is van als die tutorials bijelkaar
-    /// Da ik het niet allemaal schijven, het is er het werkt, aleen zie je het niet.
-   
-
     public class OrderController : Controller
     {
         public ActionResult Index()
         {
             return View();
         }
-
-
-
-
-
 
 
 
@@ -38,7 +29,7 @@ namespace ItVitae_RepairShop_project8.Controllers
         {
             if (ModelState.IsValid)
             {
-                ViewBag.successState = CreatePart(partModel.PartId, partModel.PartName, partModel.PartPrice);
+               // ViewBag.successState = CreatePart(partModel.PartId, partModel.PartName, partModel.PartPrice);
             }
             else
             {
@@ -46,6 +37,63 @@ namespace ItVitae_RepairShop_project8.Controllers
             }
 
             return View();
+        }
+
+        //---------------------------------------------------------------------------------------------
+        //=============================================================================================
+        //---------------------------------------------------------------------------------------------
+        public ActionResult OrderList()
+        {
+            List<OrderModel> OrdersList = new List<OrderModel>();
+            var data = OrderProcessor.Load();
+            foreach (var row in data)
+            {
+                OrdersList.Add(new OrderModel { OrderId = row.OrderId, PartLinkId = row.PartLinkId, UserId = row.UserId, EmployeeId = row.EmployeeId, OrderDiscription = row.OrderDiscription, ImageID = row.ImageID, OrderPrice = row.OrderPrice, OrderStatus = row.OrderStatus });
+            }
+
+            return View(OrdersList);
+        }
+        public ActionResult CreateOrder()
+        {
+            var x1 = DataProcessors.Create<OrderModel>(new OrderModel { Id = 6 }, "Orders");
+            var x2 = DataProcessors.Update<OrderModel>(new OrderModel { Id = 6 }, "Orders", "Id");
+            var x3 = DataProcessors.ListAll<OrderModel>(new OrderModel { Id = 6 }, "Orders");
+            var x4 = DataProcessors.ListOne<OrderModel>(new OrderModel { Id = 6 }, "Orders", "Id");
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateOrder(OrderModel orderModel)
+        {
+            OrderProcessor.Create(orderModel.PartLinkId, orderModel.UserId, orderModel.EmployeeId, orderModel.OrderDiscription, orderModel.ImageID, orderModel.OrderPrice, orderModel.OrderStatus);
+            return View();
+        }
+        public ActionResult EditOrder(int orderId)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EditOrder(OrderModel orderModel)
+        {
+            OrderProcessor.Update(orderModel.Id, orderModel.OrderId, orderModel.PartLinkId, orderModel.UserId, orderModel.EmployeeId, orderModel.OrderDiscription, orderModel.ImageID, orderModel.OrderPrice, orderModel.OrderStatus);
+            return View();
+        }
+        public ActionResult OrderDetails(int orderId)
+        {
+            var data = OrderProcessor.Load();
+            var row = (OrderModel)data.Select(o => o.OrderId == orderId);
+            var orderModelDetails = new OrderModel { 
+                OrderId = row.OrderId, 
+                PartLinkId = row.PartLinkId, 
+                UserId = row.UserId, 
+                EmployeeId = row.EmployeeId, 
+                OrderDiscription = row.OrderDiscription, 
+                ImageID = row.ImageID, 
+                OrderPrice = row.OrderPrice, 
+                OrderStatus = row.OrderStatus };
+
+
+            return View(orderModelDetails);
         }
         //---------------------------------------------------------------------------------------------
         //=============================================================================================
@@ -53,7 +101,11 @@ namespace ItVitae_RepairShop_project8.Controllers
         public ActionResult PartsList()
         {
             List<PartModel> PartsList = new List<PartModel>();
-            var data = LoadParts();
+
+            var data = PartProcessor.Load();
+            var x = data.Where(d => d.Id > 0);
+
+
             foreach (var row in data)
             {
                 PartsList.Add(new PartModel { PartId = row.PartId, PartName = row.PartName, PartPrice = row.PartPrice });
@@ -61,20 +113,41 @@ namespace ItVitae_RepairShop_project8.Controllers
 
             return View(PartsList);
         }
-        //---------------------------------------------------------------------------------------------
-        //=============================================================================================
-        //---------------------------------------------------------------------------------------------
-        public ActionResult AddPart()
+        public ActionResult CreatePart()
         {
             return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult AddPart(PartModel partModel)
+        public ActionResult CreatePart(PartModel partModel)
         {
             if(ModelState.IsValid)
             {
-                ViewBag.successState = CreatePart(partModel.PartId, partModel.PartName, partModel.PartPrice);
+                ViewBag.successState = PartProcessor.Create(partModel.PartId, partModel.PartName, partModel.PartPrice);
+            }
+            else
+            {
+                ViewBag.message = "You did it wrong >:(";
+            }
+
+            return View();
+        }
+
+        public ActionResult EditPart(int Id)
+        {
+            //Cant return object of type PartModel, omdat front and back is niet het zelfde.
+            //data uit de database is van een andere type en kan je niet uitzelzen, zonder een klote manier 
+            //Hele front en back los makeen om deze manier is alles maar niet veel makelijker. ik geef ik.
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditPart(PartModel partModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.successState = PartProcessor.Update(partModel.Id, partModel.PartId, partModel.PartName, partModel.PartPrice);
             }
             else
             {
@@ -86,17 +159,37 @@ namespace ItVitae_RepairShop_project8.Controllers
         //---------------------------------------------------------------------------------------------
         //=============================================================================================
         //---------------------------------------------------------------------------------------------
-        public ActionResult AddUser()
+        public ActionResult RegisterUser()
+        {
+            ViewBag.message = "Page not added yet!";
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RegisterUser(UserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.successState = UserProcessor.Create(userModel.UserId, userModel.UserName, userModel.UserEmail, userModel.Auth, true);
+            }
+            else
+            {
+                ViewBag.message = "You did it wrong >:(";
+            }
+
+            return View();
+        }
+        public ActionResult CreateUser()
         {
             return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult AddUser(PartModel partModel)
+        public ActionResult CreateUser(UserModel userModel)
         {
             if (ModelState.IsValid)
             {
-                ViewBag.successState = CreatePart(partModel.PartId, partModel.PartName, partModel.PartPrice);
+                ViewBag.successState = UserProcessor.Create(userModel.UserId, userModel.UserName, userModel.UserEmail, userModel.Auth, false);
             }
             else
             {
@@ -105,8 +198,58 @@ namespace ItVitae_RepairShop_project8.Controllers
 
             return View();
         }
+        public ActionResult EditUser()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EditUser(UserModel userModel)
+        {
+            UserProcessor.Update(userModel.Id, userModel.UserId, userModel.UserName, userModel.UserEmail, userModel.Auth, false);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DeleteUser(UserModel userModel)
+        {
+            UserProcessor.Remove(userModel.Id);
+            return View();
+        }
         //---------------------------------------------------------------------------------------------
         //=============================================================================================
         //---------------------------------------------------------------------------------------------
+        public ActionResult CreateEmployee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateEmployee(EmployeeModel employeeModel)
+        {
+            EmployeeProcessor.Create(employeeModel.EmployeeId, employeeModel.EmployeeEmailAddress, employeeModel.EmployeeFirstName, employeeModel.EmployeeLastName, employeeModel.EmployeePay, employeeModel.Auth);
+            return View();
+        }
+        public ActionResult EditEmployee()
+        {
+            //EmployeeProcessor.Load();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EditEmployee(EmployeeModel employeeModel)
+        {
+            EmployeeProcessor.Update(employeeModel.Id, employeeModel.EmployeeId, employeeModel.EmployeeEmailAddress, employeeModel.EmployeeFirstName, employeeModel.EmployeeLastName, employeeModel.EmployeePay, employeeModel.Auth);
+            return View();
+        }
+        public ActionResult EmployeesList()
+        {
+            List<EmployeeModel> EmployeesList = new List<EmployeeModel>();
+            var data = EmployeeProcessor.Load();
+            foreach (var row in data)
+            {
+                EmployeesList.Add(new EmployeeModel { EmployeeId = row.EmployeeId, EmployeeEmailAddress = row.EmployeeEmailAddress, EmployeeFirstName = row.EmployeeFirstName, EmployeeLastName = row.EmployeeLastName, EmployeePay = row.EmployeePay, Auth = row.Auth });
+            }
+
+            return View(EmployeesList);
+        }
     }
 }
---
+++ Models/ViewModels.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ItVitae_RepairShop_project8.Models
+{
+    public class ViewModels
+    {
+        
+    }
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserEmailConfirm { get; set; }
+        public int Auth { get; set; }
+        public string UserPassword { get; set; } 
+    }
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public string EmployeeEmailAddress { get; set; }
+        public string EmployeeFirstName { get; set; }
+        public string EmployeeLastName { get; set; }
+        public double EmployeePay { get; set; }
+        public int Auth;
+    }
+    public class PartModel
+    {
+        public int Id { get; set; }
+        public int PartId { get; set; }
+        public string PartName { get; set; }
+        public double PartPrice { get; set; }
+    }
+    public class PartLinkModel
+    {
+        public int Id { get; set; }
+        public int PartId { get; set; }
+    }
+    public class ImageModel
+    {
+        public int Id { get; set; }
+        public int ImageId { get; set; }
+        public string ImageData { get; set; }
+    }
+    public class ImageLinkModel
+    {
+        public int Id { get; set; }
+        public int ImageId { get; set; }
+        public int OrderId { get; set; }
+    }
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int PartLinkId { get; set; }
+        public int UserId { get; set; }
+        public int EmployeeId { get; set; }
+        public string OrderDiscription { get; set; }
+        public int ImageID { get; set; }
+        public double OrderPrice { get; set; }
+        public int OrderStatus { get; set; }
+    }
+}
